ace69cfcbfa908eed3593dc61387a9d8604ee223
julita.gajewska <j.gajewska.4@gmail.com>
2023-08-12 23:09:15 +0200

    feat: react and vue i18n configuration

--- apps/client-react/src/views/general/HomeView.tsx
+++ apps/client-react/src/views/general/HomeView.tsx
@@ -1,5 +1,8 @@
+import { useTranslation } from 'react-i18next';
+
 const HomeView = () => {
-  return <div className="h-[200vh] w-full bg-slate-200 rounded-xl">Main dashboard</div>;
+  const { t } = useTranslation();
+  return <div className="h-[200vh] w-full bg-slate-200 rounded-xl">{t('page.title.home')}</div>;
 };
 
 export default HomeView;
